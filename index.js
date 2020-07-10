e9582de48b65fccbddd3aaa14a264b995da80451
David <sc@silvercrow.org>
2020-07-09 19:02:01 -0600

    updating webhook params

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Require express and body-parser
 const express = require("express")
 const bodyParser = require("body-parser")
+const nodemailer = require('nodemailer');
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "bccdeac70ebeef",
+    pass: "cd566b38baf0b9"
+  }
+});
 
 
 // Initialize express and define a port
@@ -14,12 +23,23 @@ app.use(bodyParser.json());
 
 
 app.post("/hook", (req, res) => {
- var data = JSON.parse(Object.keys(req.body)[0]);
-console.log('name:',data.name,'email:',data.email,'message:',data.message,'id:',data.id);
+  var data = JSON.parse(Object.keys(req.body)[0]);
+  console.log('name:',data.name,'email:',data.email,'message:',data.message,'id:',data.id, 'subject:',data.subject);
 
-//console.log(JSON.stringify(req.body))
-//console.log(JSON.parse(req.body))
-
+  const html = 'Article id is:'+data.id;
+  const mailOptions = {
+    from: '"Autoweb" <aceTest@autoweb.com>',
+    to: data.email,
+    subject: data.subject,
+    text: data.message,
+    html: `<b>${data.subject}</b> ${data.message} <br/>   ${html}<br />`
+  };
+  transport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+  });
 
   res.status(200).end() // Responding is important
 })
